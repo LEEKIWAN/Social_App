8d0c8320020fcf9c28306965f68e16592e5a8e37
이기완 <kiwan.lee@waiker.ai>
2023-09-19 16:52:34 +0900

    [Feat] Impressive User Profile Details UI

---
+++ Social_App/Common/Extension/View+.swift
@@ -0,0 +1,8 @@
+//
+//  View+.swift
+//  Social_App
+//
+//  Created by 이기완 on 2023/09/19.
+//
+
+import Foundation
---
+++ Social_App/Views/User/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  Social_App
+//
+//  Created by 이기완 on 2023/09/19.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
